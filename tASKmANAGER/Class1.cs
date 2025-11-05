e177838445f700401853f8e9192c6284c9ab3580
Timerkhan5 <tkurtashkin05@gmail.com>
2025-11-06 00:45:30 +0300

    Добавьте файлы проекта.

---
+++ tASKmANAGER/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tASKmANAGER
+{
+    enum Status
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+    class Task
+    {
+        public static int Id { get; set; }
+        public string Description { get; set; }
+        public Status TaskStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Task() { }
+        public Task(string description)
+        {
+            Id++;
+            Description = description;
+            TaskStatus = Status.ToDo;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+        public void updateDescription(string newDescription)
+        {
+            Description = newDescription;
+            UpdatedAt = DateTime.Now;
+        }
+        public void updateStatus(Status newStatus)
+        {
+            TaskStatus = newStatus;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
